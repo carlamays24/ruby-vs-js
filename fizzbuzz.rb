b4b10431a166ad815ffaf771a7535b678d053911
carlamays24@gmail.com <providenceri@gmail.com>
2016-04-11 07:00:08 -0400

    ....

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,21 @@
 require 'pry'
 
 # Let `max_num` be the number that you want to count to.
+
+max_num = 100;
+i = 0;
+
+while i < max_num do
+   i+=1
+   if i % 15 == 0
+       puts "fizzbuzz"
+   elsif i % 3 == 0
+      puts "fizz"
+   elsif i % 5 == 0
+       puts "buzz"
+  else
+  end
+end
+
+binding.pry
+""
